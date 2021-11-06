4df171ec846d0ec70664d56e10b6a5e158c885bf
Marko <marko.koprivanac.19@singimail.rs>
2021-11-06 20:27:44 +0100

    baze, sređena pozadina malo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
   <div class="app">
-    <p>Kocka je pala na: {{ DiceNumber }}</p>
-    <button @click="RollDice">Baci kockicu</button>
-    <img v-for="(Path, Index) in ImagePaths" v-bind:key="Index" :src="Path" :alt="'Player ' + Index" /> <!-- require('@/assets/images/players/' + Index + '.png')" -->
-  </div>
+    <div class="dice">
+      <p>Kocka je pala na: {{ DiceNumber }}</p>
+      <button @click="RollDice">Baci kockicu</button>
+    </div>
+    <div class="field-background">
+      <div class="player-1-base">
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+      </div>
+      <div class="player-2-base">
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+      </div>
+      <div class="player-3-base">
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+      </div>
+      <div class="player-4-base">
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+        <div class="base"></div>
+      </div>
+      <div
+        class="img"
+        v-for="(Image, Index) in PlayerImages"
+        v-bind:key="Index"
+      >
+        <img
+          :src="require(`@/assets/images/players/${Image}.png`)"
+          :alt="'Player ' + Index"
+          :class="'player' + Index"
+        />
+        <img
+          :src="require(`@/assets/images/players/${Image}.png`)"
+          :alt="'Player ' + Index"
+          :class="'player' + Index"
+        />
+        <img
+          :src="require(`@/assets/images/players/${Image}.png`)"
+          :alt="'Player ' + Index"
+          :class="'player' + Index"
+        />
+        <img
+          :src="require(`@/assets/images/players/${Image}.png`)"
+          :alt="'Player ' + Index"
+          :class="'player' + Index"
+        />
+        <!-- require('@/assets/images/players/' + Index + '.png')" -->
+      </div>
+    </div>
+  </div> 
 </template>
 
 <script lang="ts">
@@ -18,12 +72,7 @@ export default defineComponent({
     return {
       StartingPositions: [1, 13, 25, 37], // Startne pozicije po bojama(igračima)
       DiceNumber: 0, // Broj koji je bacen
-      ImagePaths: [ // Putanje do slika igrača
-        "@/assets/images/players/1.png",
-        "@/assets/images/players/2.png",
-        "@/assets/images/players/3.png",
-        "@/assets/images/players/4.png"
-      ],
+      PlayerImages: ["yellow", "blue", "green", "red"],
     };
   },
   mounted() {
@@ -43,7 +92,7 @@ export default defineComponent({
       Field.style.borderRadius = "50%";
 
       // Brojevi polja, za debug možda posle neka upotreba
-      Field.innerHTML = `${i}`;
+      //Field.innerHTML = `${i}`;
 
       // Prvi red, svaki sledeći if predstavlja jedan red
       if (i > 0 && i < 7) {
@@ -95,11 +144,15 @@ export default defineComponent({
         Field.style.left = "280px";
       }
 
+      if (this.StartingPositions.includes(i)) {
+        Field.style.background = "#00000030"
+      }
+
       // Dodajemo element
       const Main = document.getElementById("app");
       if (Main) Main.append(Field);
 
-      // Pushamo polja u array da možemo posle da im pristupimo
+      // Pushamo polja u array, možda zatreba
       FieldsCollection.push(Field);
     }
   },
@@ -107,6 +160,13 @@ export default defineComponent({
   methods: {
     RollDice() {
       this.DiceNumber = Math.floor(Math.random() * 6) + 1;
+    },
+
+    Move(Player: number, Value: number) {
+      if (Value < 0 || Value > FieldsCollection.length)
+        return alert("Neispravan broj polja." + "0-" + FieldsCollection.length);
+
+      const Field = FieldsCollection[Value];
     },
   },
 });
@@ -116,5 +176,109 @@ export default defineComponent({
 #app {
   width: 100%;
   height: 100%;
+}
+
+.dice {
+  position: absolute;
+  left: 600px;
+  top: 40px;
+}
+
+.field-background {
+  position: relative;
+  width: 528px;
+  height: 535px;
+  margin: 40px 26px;
+  border: 5px ridge gray;
+  padding: 0px;
+}
+
+.player-1-base {
+  position: absolute;
+  top: 40px;
+  left: 40px;
+  width: 140px;
+  height: 140px;
+  background: yellow;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+}
+
+.player-2-base {
+  position: absolute;
+  top: 40px;
+  right: 40px;
+  width: 140px;
+  height: 140px;
+  background: blue;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+}
+
+.player-3-base {
+  position: absolute;
+  bottom: 40px;
+  right: 40px;
+  width: 140px;
+  height: 140px;
+  background: red;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+}
+
+.player-4-base {
+  position: absolute;
+  bottom: 40px;
+  left: 40px;
+  width: 140px;
+  height: 140px;
+  background: green;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+}
+
+.base {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin: 0 auto;
+  background: #00000090;
+}
+
+.player0 {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.player1 {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
+.player2 {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+}
+.player3 {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
 }
 </style>
